e5a36efdb83319ce8e0c04ed8552f768f1ae0f2c
Rajan Somani <rajan.somani1212@gmail.com>
2019-05-03 17:43:10 +0530

    first commit ss

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ app.get('/', (request, response) => {
 })
 
 
-app.get('/users', db.getUsers)
-app.get('/getAllRetailers', db.getAllRetailers)
-app.get('/getAllRetailersById/:id', db.getRetailerById)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+// app.get('/users', db.getUsers)
+// app.get('/getAllRetailers', db.getAllRetailers)
+// app.get('/getAllRetailersById/:id', db.getRetailerById)
+// app.get('/users/:id', db.getUserById)
+// app.post('/users', db.createUser)
+// app.put('/users/:id', db.updateUser)
+// app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
